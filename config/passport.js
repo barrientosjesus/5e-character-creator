d0ec20777660daf4ddbdd3153e48a00ea40103af
barrientosjesus <jabarri1@gmail.com>
2023-10-09 17:04:24 -0700

    Add testing for blizzard login

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const BnetStrategy = require('passport-bnet').Strategy;
 
 const User = require('../models/user');
 
@@ -22,6 +23,34 @@ passport.use(new GoogleStrategy(
                 googleId: profile.id,
                 email: profile.emails[0].value,
                 avatar: profile.photos[0].value
+            });
+        } catch (err) {
+            return cb(err);
+        }
+    }
+));
+
+passport.use(new BnetStrategy(
+    {
+        clientID: process.env.BNET_ID,
+        clientSecret: process.env.BNET_SECRET,
+        scope: process.env.OAUTH_SCOPES || "wow.profile",
+        callbackURL: process.env.OAUTH_CALLBACK_URL || "http://localhost:3000/oauth/battlenet/callback",
+        region: "us"
+    },
+
+    async function (accessToken, refreshToken, profile, cb) {
+
+        try {
+            let user = await User.findOne({ googleId: profile.id });
+
+            if (user) return cb(null, user);
+            console.log('try BNET STRAT')
+            user = await User.create({
+                name: profile.battletag,
+                googleId: profile.id,
+                email: "test@gmail.com",
+                avatar: "https://png.pngtree.com/element_our/20200610/ourmid/pngtree-character-default-avatar-image_2237203.jpg"
             });
         } catch (err) {
             return cb(err);
--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,52 @@ router.get('/oauth2callback', function (req, res, next) {
   )(req, res, next);  // Call the middleware returned by passport
 });
 
+router.get('/oauth/battlenet',
+  passport.authenticate('bnet'));
+
+router.get('/oauth/battlenet/callback',
+  passport.authenticate('bnet', { failureRedirect: '/' }),
+  function (req, res) {
+    res.redirect('/');
+  });
+
+router.get('/blizzaccount', function (req, res) {
+  if (req.isAuthenticated()) {
+    const output = `
+      <html>
+        <body>
+          <h1>Express Passport-Bnet OAuth Example</h1>
+          <table>
+            <tr>
+              <th>Account ID</th>
+              <th>Battletag</th>
+            </tr>
+            <tr>
+              <td>${req.user.googleId}</td>
+              <td>${req.user.name}</td>
+            </tr>
+          </table>
+          <br />
+          <a href="/logout">Logout</a>
+        </body>
+      </html>
+    `;
+    res.send(output);
+  } else {
+    const output = `
+      <html>
+        <body>
+          <h1>Express Passport-Bnet OAuth Example</h1>
+          <br />
+          <a href="/oauth/battlenet">Login with Bnet</a>
+        </body>
+      </html>
+    `;
+    res.send(output);
+  }
+});
+
+
 router.get('/logout', function (req, res) {
   req.logout(function () {
     res.redirect('/characters');
